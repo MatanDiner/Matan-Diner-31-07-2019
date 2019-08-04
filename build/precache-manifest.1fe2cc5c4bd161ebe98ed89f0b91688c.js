f569fab1d7be51fd28cbfe3eb93f2fa28a26da17
MatanDiner <33176241+MatanDiner@users.noreply.github.com>
2019-08-04 21:04:47 +0300

    Add files via upload

---
+++ build/precache-manifest.1fe2cc5c4bd161ebe98ed89f0b91688c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67f571e245f3109636f74c458b20b7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2624a1a6e9c5995e28fa",
+    "url": "/static/css/main.bfe18145.chunk.css"
+  },
+  {
+    "revision": "568c3c6c19db482bf933",
+    "url": "/static/js/2.f1acf5fe.chunk.js"
+  },
+  {
+    "revision": "2624a1a6e9c5995e28fa",
+    "url": "/static/js/main.01e3048b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
