8812301e77048cdd5312b363b18100da8e5bb7b8
MatanDiner <33176241+MatanDiner@users.noreply.github.com>
2019-08-05 23:46:13 +0300

    Update Favorite.js

--- src/containers/FavoritesList/Favorite/Favorite.js
+++ src/containers/FavoritesList/Favorite/Favorite.js
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
 import classes from './Favorite.css';
 import HeaderButtonsContext from '../../../Contexts/headerButtonsContext';
-
+import FaInfo from 'react-icons/lib/fa/info';
+import FaTrash from 'react-icons/lib/fa/trash';
+import { Link } from 'react-router-dom';
 const Favorite = props => {
 
     const context = useContext(HeaderButtonsContext);
@@ -10,8 +12,8 @@ const Favorite = props => {
     if (!context.screenMode) {
         classesArr = [classes.Favorite, classes.LightFavorite];
     }
-
     return (
+
         <div className={classesArr.join(" ")}>
             <div className={classes.Temp}>
                 <label className={classes.Label}>{props.name}</label>
@@ -20,7 +22,16 @@ const Favorite = props => {
             <div>
                 <label className={classes.WeatherText}>{props.weatherText}</label>
             </div>
+            <div className={classes.containericons}>
+                <div className={classes.iconDiv}>
+                    <Link className={classes.Link} to={`/${props.locationKey}/${props.name}`}>
+                        <FaInfo className={classes.infoIcon} />
+                    </Link>
+                    <FaTrash className={classes.deleteIcon} onClick={() => props.removeFromFavorite(props.locationKey)} />
+                </div>
+            </div>
         </div>
+
     );
 
 }
